5ab95df8ba3142140681d49ff6b0ae5d60a0728d
권은서 <kwonenseo@gmail.com>
2022-06-05 14:38:25 +0900

    Feat: Create footer component

---
+++ src/components/QuestionCard/index.tsx
@@ -0,0 +1,10 @@
+const QuestionCard = () => {
+  return (
+    <div>
+      <h1>사람들이 자꾸 쳐다보는데..왜지?</h1>
+      <button type='button'>잘생긴 건 알아가지구 ㅋ</button>
+      <button type='button'>뭐지..얼굴에 뭐 묻었나?</button>
+    </div>
+  )
+}
+export default QuestionCard
