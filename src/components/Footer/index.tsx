b95042d4c1ef208003e7ee90eed693a8d969ce15
권은서 <kwonenseo@gmail.com>
2022-06-12 13:30:53 +0900

    Modify: Change the button name about mode

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,44 @@
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+import { modeState } from 'utils/atoms'
 import styles from './footer.module.scss'
 
 const Footer = () => {
   const navigate = useNavigate()
+  const mode = useRecoilValue(modeState)
+  const currentMode = () => {
+    switch (mode) {
+      case 'cat':
+        return '냥BTI'
+      case 'dog':
+        return '멍BTI'
+      case 'chart':
+        return '멍냥MBTI'
+    }
+    return '냥BTI'
+  }
+
+  const handleClickBtn = () => {
+    switch (mode) {
+      case 'cat':
+        navigate('/catMode')
+        break
+      case 'dog':
+        navigate('/dogMode')
+        break
+      case 'chart':
+        navigate('/')
+        break
+    }
+  }
+
   return (
     <footer className={styles.footer}>
       <button type='button' onClick={() => navigate(-1)}>
         <span className='material-symbols-outlined'>west </span>
       </button>
-      <button type='button' onClick={() => navigate('/catMode')} className={styles.mainBtn}>
-        냥BTI
+      <button type='button' onClick={handleClickBtn} className={styles.mainBtn}>
+        {currentMode()}
       </button>
       <button type='button' onClick={() => navigate('/')}>
         <span className='material-symbols-outlined'>home </span>
