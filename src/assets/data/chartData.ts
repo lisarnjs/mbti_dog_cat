96dee29ee81d35950b3936ab8f90ff7c0b572078
권은서 <kwonenseo@gmail.com>
2022-06-14 19:11:07 +0900

    Add data of mbti rate in Korea

---
+++ src/assets/data/chartData.ts
@@ -0,0 +1,25 @@
+interface chartDataProps {
+  mbti: string
+  rate: number
+}
+
+const chartData: chartDataProps[] = [
+  { mbti: 'ISFJ', rate: 7.66 },
+  { mbti: 'ISFP', rate: 6.61 },
+  { mbti: 'ISTJ', rate: 4.28 },
+  { mbti: 'ISTP', rate: 3.11 },
+  { mbti: 'INFP', rate: 13.39 },
+  { mbti: 'INFJ', rate: 6.25 },
+  { mbti: 'INTJ', rate: 3.75 },
+  { mbti: 'INTP', rate: 6.28 },
+  { mbti: 'ESFJ', rate: 8.35 },
+  { mbti: 'ESFP', rate: 6.36 },
+  { mbti: 'ESTJ', rate: 4.56 },
+  { mbti: 'ESTP', rate: 2.94 },
+  { mbti: 'ENFP', rate: 12.6 },
+  { mbti: 'ENFJ', rate: 6.09 },
+  { mbti: 'ENTP', rate: 5.04 },
+  { mbti: 'ENTJ', rate: 2.73 },
+]
+
+export default chartData
